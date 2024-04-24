e9761590c965cf02dc20c9d06d70b1a2691f1af6
VenuDhanekula <46818954+VenuDhanekula@users.noreply.github.com>
2024-04-25 00:49:53 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,17 +6,25 @@ function darkModeToggle() {
 
 function insert_value(){
     var scriptURL = "https://script.google.com/macros/s/AKfycbzKSzQHu5DLCLSpWxPfhWqeeNfINo3VutcvDnjX9CkCEFWr92mstAWcwNIHSKsYEaVKug/exec"
-    var name = document.getElementById("name").value; 
-    console.log(name);
+    
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value; 
+    var email = document.getElementById("email").value; 
+    var message = document.getElementById("message").value; 
+    
+    console.log(name, phone, email, message);
 
     scriptURL = scriptURL + "?name="+name;
+    scriptURL = scriptURL + "&phone="+phone;
+    scriptURL = scriptURL + "&email="+email;
+    scriptURL = scriptURL + "&message="+message;
 
     var delayInMilliseconds = 500;
     mywindow = window.open(scriptURL,"", "width=500,height=300");
 
     setTimeout(function() {
         mywindow.close();
-        alert('Thank You! Will Get In Touch Shortly');
+        //alert('Thank You! Will Get In Touch Shortly');
       }, delayInMilliseconds);
 
     document.getElementById("contactForm").reset();
