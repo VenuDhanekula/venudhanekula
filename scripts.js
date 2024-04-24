e88e7734d41c89c1579d154f0084fa2d71bce870
VenuDhanekula <46818954+VenuDhanekula@users.noreply.github.com>
2024-04-25 00:04:29 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,21 @@ function darkModeToggle() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+
+function insert_value(){
+    var scriptURL = "https://script.google.com/macros/s/AKfycbzKSzQHu5DLCLSpWxPfhWqeeNfINo3VutcvDnjX9CkCEFWr92mstAWcwNIHSKsYEaVKug/exec"
+    var name = document.getElementById("name").value; 
+    console.log(name);
+
+    scriptURL = scriptURL + "?name="+name;
+
+    var delayInMilliseconds = 500;
+    mywindow = window.open(scriptURL,"", "width=500,height=300");
+
+    setTimeout(function() {
+        mywindow.close();
+      }, delayInMilliseconds);
+
+    document.getElementById("contactForm").reset();
+  }
